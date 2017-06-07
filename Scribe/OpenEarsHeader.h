faffad556adc8e898b7953a27e4e3d04ebdd6549
Timothy Tran <tttran@vt.edu>
2017-06-07 18:22:02 -0400

    add openears stuff

---
+++ Scribe/OpenEarsHeader.h
@@ -0,0 +1,20 @@
+//
+//  OpenEarsHeader.h
+//  Scribe
+//
+//  Created by Tran, Timothy on 6/7/17.
+//  Copyright © 2017 Tran, Timothy. All rights reserved.
+//
+
+#ifndef OpenEarsHeader_h
+#define OpenEarsHeader_h
+
+#import <OpenEars/OEPocketsphinxController.h>
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEFliteController.h>
+#import <Slt/Slt.h> // Only needed if using Flite speech
+#import <OpenEars/OEEventsObserver.h>
+#import <OpenEars/OELogging.h>
+#import <OpenEars/OEAcousticModel.h>
+
+#endif /* OpenEarsHeader_h */
--- Scribe/ViewController.swift
+++ Scribe/ViewController.swift
@@ -11,6 +11,7 @@ import Speech
 import AVFoundation
 
 
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
@@ -28,9 +29,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         activitySpinner.isHidden = true
         transcriptionTextField.isEditable = false
-
+        
+        let lmGenerator = OELanguageModelGenerator()
+        let words = ["word", "Statement", "other word", "A PHRASE"] // These can be lowercase, uppercase, or mixed-case.
+        let name = "NameIWantForMyLanguageModelFiles"
+        let err: Error! = lmGenerator.generateLanguageModel(from: words, withFilesNamed: name, forAcousticModelAtPath: OEAcousticModel.path(toModel: "AcousticModelEnglish"))
+        
+        if(err != nil) {
+            print("Error while creating initial language model: \(err)")
+        } else {
+            let lmPath = lmGenerator.pathToSuccessfullyGeneratedLanguageModel(withRequestedName: name) // Convenience method to reference the path of a language model known to have been created successfully.
+            let dicPath = lmGenerator.pathToSuccessfullyGeneratedDictionary(withRequestedName: name) // Convenience method to reference the path of a dictionary known to have been created successfully.
+        }
+        
     }
 
+    
+    
+    
+    
     
     @IBAction func playBtnPressed(_ sender: CircleButton) {
         if (recording == false) {
