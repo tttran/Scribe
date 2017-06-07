4553041b25638acd16a28b6017a5ab10eeb27b65
Timothy Tran <tttran@vt.edu>
2017-06-07 13:37:40 -0400

    Initial Commit

---
+++ Scribe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scribe
+//
+//  Created by Tran, Timothy on 6/7/17.
+//  Copyright © 2017 Tran, Timothy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
