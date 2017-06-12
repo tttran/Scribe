3926bf350eb7f013bd8a4837b7a1f0a4527cccc4
Timothy Tran <tttran@vt.edu>
2017-06-12 11:21:49 -0400

    working prototype for sending money via scribe

--- Extension/IntentHandler.swift
+++ Extension/IntentHandler.swift
@@ -28,7 +28,19 @@ class IntentHandler: INExtension {
     
     
     
-    
+
 }
 
 
+extension IntentHandler: INSendPaymentIntentHandling {
+    
+    func handle(sendPayment intent: INSendPaymentIntent, completion: @escaping (INSendPaymentIntentResponse) -> Void) {
+        // Check that we have valid values for payee and currencyAmount
+        guard let payee = intent.payee, let amount = intent.currencyAmount else {
+            return completion(INSendPaymentIntentResponse(code: .unspecified, userActivity: nil))
+        }
+        // Make your payment!
+        print("Sending \(amount) payment to \(payee)!")
+        completion(INSendPaymentIntentResponse(code: .success, userActivity: nil))
+    }
+}
